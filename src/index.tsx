1edddddb3a08aa29a14ac4b8bd8c0dc4d7158bd1
Morten Jansrud <morten.jansrud@nav.no>
2020-09-04 15:44:42 +0200

    Fjern ugyldig tegn

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import EnforceLoginLoader, { Auth } from './components/EnforceLoginLoader'
 import { setBreadcrumbs } from './functions/breadcrumbs'
 import { setAvailableLanguages } from './functions/languages'
-export { EnforceLoginLoader, Auth, setBreadcrumbs, setAvailableLanguagesgi }
+export { EnforceLoginLoader, Auth, setBreadcrumbs, setAvailableLanguages }
