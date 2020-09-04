c207a1c4e9abad14007765d87d812b94924ffc1a
Morten Jansrud <morten.jansrud@nav.no>
2020-09-04 15:43:58 +0200

    Korriger navn på funksjon

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import EnforceLoginLoader, { Auth } from './components/EnforceLoginLoader'
 import { setBreadcrumbs } from './functions/breadcrumbs'
-import { setLanguages } from './functions/languages'
-export { EnforceLoginLoader, Auth, setBreadcrumbs, setLanguages }
+import { setAvailableLanguages } from './functions/languages'
+export { EnforceLoginLoader, Auth, setBreadcrumbs, setAvailableLanguagesgi }
