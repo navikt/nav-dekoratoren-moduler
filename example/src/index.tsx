55a4628c9dbbaf1260c3972e8ac99e4c3a724438
Morten Jansrud <morten.jansrud@nav.no>
2020-09-10 16:28:54 +0200

    Oppdater eksempel

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -18,7 +18,7 @@ document.body.innerHTML = document.body.innerHTML.replace(
 );
 document.body.innerHTML = document.body.innerHTML.replace(
   "{{{SCRIPTS}}}",
-  `<div id="decorator-env" data-src="http://localhost:8088/dekoratoren/env?enforceLogin=True&level=Level4"></div>`
+  `<div id="decorator-env" data-src="http://localhost:8088/dekoratoren/env?enforceLogin=true&level=Level4"></div>`
 );
 
 // Execute client.js
