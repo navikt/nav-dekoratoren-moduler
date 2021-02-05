8136330788dadcbb94c7a64d0483f0133c6a9e13
Morten Jansrud <morten.jansrud@nav.no>
2021-02-05 10:54:57 +0100

    Legg til url-lookup-table

---
+++ src/url-lookup-table/utils.ts
@@ -0,0 +1,19 @@
+import { urlLookupTable } from './table'
+
+export const getUrlFromLookupTable = (
+  url: string,
+  env: 'dev' | 'q0' | 'q1' | 'q2' | 'q6'
+) => {
+  let match
+  const lookupTable = urlLookupTable[env]
+  if (url && lookupTable) {
+    Object.keys(lookupTable).some((key) => {
+      if (url.startsWith(key)) {
+        match = key
+        return true
+      }
+      return false
+    })
+  }
+  return match ? url.replace(match, lookupTable[match]) : url
+}
