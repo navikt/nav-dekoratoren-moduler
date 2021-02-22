b3adf51f69938fbe7aee4b4273f1618e20d112c6
Morten Jansrud <morten.jansrud@nav.no>
2021-02-22 15:37:04 +0100

    Legg til en HTML-parser

--- src/functions/ssr.tsx
+++ src/functions/ssr.tsx
@@ -1,14 +1,17 @@
 import React, { Fragment } from 'react';
 import { JSDOM } from 'jsdom';
+import fetch from 'node-fetch';
+import NodeCache from 'node-cache';
+import HtmlParser from 'react-html-parser';
 import { FunctionComponent } from 'react';
 import { ENV, getDekoratorUrl } from './utils';
 import { Params } from './params';
 
 export interface Elements {
-  styles: HTMLElement;
-  scripts: HTMLElement;
-  header: HTMLElement;
-  footer: HTMLElement;
+  styles: string;
+  scripts: string;
+  header: string;
+  footer: string;
 }
 
 export interface Components {
@@ -24,51 +27,49 @@ export const fetchDecoratorReact = async (
 ): Promise<Components> => {
   const elements = await fetchDecoratorHtml(env, params);
   return {
-    Styles: () => <Fragment>{createReactElements(elements.styles)}</Fragment>,
-    Scripts: () => <Fragment>{createReactElements(elements.scripts)}</Fragment>,
-    Header: () => (
-      <div dangerouslySetInnerHTML={{ __html: elements.header.innerHTML }} />
-    ),
-    Footer: () => (
-      <div dangerouslySetInnerHTML={{ __html: elements.footer.innerHTML }} />
-    )
+    Styles: () => <Fragment>{HtmlParser(elements.styles)}</Fragment>,
+    Scripts: () => <Fragment>{HtmlParser(elements.scripts)}</Fragment>,
+    Header: () => <Fragment>{HtmlParser(elements.header)}</Fragment>,
+    Footer: () => <Fragment>{HtmlParser(elements.footer)}</Fragment>
   };
 };
 
-export const fetchDecoratorHtml = async (
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+
+// Refresh cache every hour
+const cache = new NodeCache({
+  stdTTL: SECONDS_PER_HOUR,
+  checkperiod: SECONDS_PER_MINUTE
+});
+
+export const fetchDecoratorHtml = (
   env: ENV,
   params?: Params
 ): Promise<Elements> => {
   const url = getDekoratorUrl(env, params);
-  const res = await fetch(url);
-  const body = await res.text();
-
-  const { document } = new JSDOM(body).window;
-  const styles = document.getElementById('styles');
-  const scripts = document.getElementById('scripts');
-  const header = document.getElementById('header-withmenu');
-  const footer = document.getElementById('footer-withmenu');
-
-  if (!styles || !scripts || !header || !footer) throw new Error(body);
-  return { styles, scripts, header, footer };
-};
-
-const createReactElements = (element: Element) =>
-  Object.values(element.children).map((element, key) =>
-    createReactElement(element, key)
-  );
-
-const createReactElement = (element: Element, key: number) => {
-  const tagName = element.tagName.toLowerCase();
-  const attributes = Object.fromEntries(
-    Object.values(element.attributes).map((a) => [a.name, a.value])
-  );
-
-  attributes.key = `${tagName}-${key}`;
-  if (tagName === 'script') {
-    // @ts-ignore
-    attributes.async = true;
+  const cacheData = cache.get(url);
+  if (cacheData) {
+    return new Promise((resolve) => resolve(cacheData as Elements));
   }
 
-  return React.createElement(tagName, attributes);
+  return fetch(url)
+    .then((res) => {
+      return res.text();
+    })
+    .then((res) => {
+      const { document } = new JSDOM(res).window;
+      const styles = document.getElementById('styles')?.innerHTML;
+      const scripts = document.getElementById('scripts')?.innerHTML;
+      const header = document.getElementById('header-withmenu')?.innerHTML;
+      const footer = document.getElementById('footer-withmenu')?.innerHTML;
+
+      if (!header || !footer || !styles || !scripts) {
+        throw new Error("'Elements doesn't exist");
+      }
+
+      const elements = { styles, scripts, header, footer };
+      cache.set(url, elements);
+      return elements;
+    });
 };
