b352881c796d0bf47eae42ed61d89cbc5aeb5f69
Morten Jansrud <morten.jansrud@nav.no>
2021-02-24 16:44:49 +0100

    Refaktor

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { EnforceLoginLoader, Auth } from '@navikt/nav-dekoratoren-moduler';
-import { onLanguageSelect } from '@navikt/nav-dekoratoren-moduler';
-import { setAvailableLanguages } from '@navikt/nav-dekoratoren-moduler';
-import { onBreadcrumbClick } from '@navikt/nav-dekoratoren-moduler';
-import { setBreadcrumbs } from '@navikt/nav-dekoratoren-moduler';
-import { setParams } from '@navikt/nav-dekoratoren-moduler';
+import { useState } from "react";
+import { EnforceLoginLoader, Auth } from "@navikt/nav-dekoratoren-moduler";
+import { onLanguageSelect } from "@navikt/nav-dekoratoren-moduler";
+import { setAvailableLanguages } from "@navikt/nav-dekoratoren-moduler";
+import { onBreadcrumbClick } from "@navikt/nav-dekoratoren-moduler";
+import { setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
+import { setParams } from "@navikt/nav-dekoratoren-moduler";
 
 const App = () => {
   const initalParams = { simple: true };
@@ -12,7 +12,7 @@ const App = () => {
   const [localParams, setLocalParams] = useState<string>(
     JSON.stringify(initalParams, null, 2)
   );
-  const [activeLanguage, setActiveLanguage] = useState('nb');
+  const [activeLanguage, setActiveLanguage] = useState("nb");
 
   onBreadcrumbClick((breadcrumb) => {
     console.log(breadcrumb);
@@ -38,34 +38,33 @@ const App = () => {
     console.log(auth);
     setBreadcrumbs([
       {
-        title: 'Ditt nav',
-        url: 'https://www.nav.no/person/dittnav',
-        handleInApp: true
+        title: "Ditt nav",
+        url: "https://www.nav.no/person/dittnav",
+        handleInApp: true,
       },
       {
-        title: 'Kontakt oss',
-        url: 'https://www.nav.no/person/kontakt-oss',
-        handleInApp: true
-      }
+        title: "Kontakt oss",
+        url: "https://www.nav.no/person/kontakt-oss",
+        handleInApp: true,
+      },
     ]);
     setAvailableLanguages([
-      { locale: 'nb', url: 'https://www.nav.no/no/person', handleInApp: true },
-      { locale: 'en', url: 'https://www.nav.no/en/person', handleInApp: true }
+      { locale: "nb", url: "https://www.nav.no/no/person", handleInApp: true },
+      { locale: "en", url: "https://www.nav.no/en/person", handleInApp: true },
     ]);
   };
 
   // console.log(header);
 
   return (
-    <EnforceLoginLoader authCallback={authCallback}
-    >
-      <div className={'testapp__container'}>
+    <EnforceLoginLoader authCallback={authCallback}>
+      <div className={"testapp__container"}>
         <div>Test app</div>
         <div>
           <textarea
             value={localParams}
-            className={'testapp__textarea'}
-            placeholder={'params'}
+            className={"testapp__textarea"}
+            placeholder={"params"}
             onChange={(e) => setLocalParams(e.target?.value)}
           />
         </div>
