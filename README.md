4432411d8ff1070492a4f1e3faeb624ff611ca23
Morten Jansrud <morten.jansrud@nav.no>
2021-03-10 16:28:31 +0100

    Tillat undefined

--- README.md
+++ README.md
@@ -115,6 +115,7 @@ export interface Params {
   language?: 'nb' | 'nn' | 'en' | 'se'
   availableLanguages?: Language[]
   breadcrumbs?: Breadcrumb[]
+  utilsBackground?: "white" | "gray" | undefined
   feedback?: boolean
   chatbot?: boolean
 }
--- src/csr/functions/params.ts
+++ src/csr/functions/params.ts
@@ -11,7 +11,7 @@ export interface Params {
   language?: "nb" | "nn" | "en" | "se" | "pl";
   availableLanguages?: Language[];
   breadcrumbs?: Breadcrumb[];
-  utilsBackground?: "white" | "gray";
+  utilsBackground?: "white" | "gray" | undefined;
   feedback?: boolean;
   chatbot?: boolean;
 }
