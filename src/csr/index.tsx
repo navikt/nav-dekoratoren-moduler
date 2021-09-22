1209555776b2cde9e2d040d959d90163c7c1cbd7
anders <anders@nomerstad.com>
2021-09-22 15:57:00 +0200

    Fjerner unødvendiger imports/exports

--- src/csr/index.tsx
+++ src/csr/index.tsx
@@ -5,7 +5,6 @@ import { injectDecoratorClientSide } from "./functions/csr";
 import { getUrlFromLookupTable } from "./url-lookup-table/utils";
 import { urlLookupTable } from "./url-lookup-table/table";
 import { setParams } from "./functions/params";
-import { Breadcrumb, Language, Params } from "../common/common-types";
 
 export {
   EnforceLoginLoader,
@@ -19,4 +18,4 @@ export {
   injectDecoratorClientSide,
 };
 
-export type { Breadcrumb, Language, Auth, Params };
+export type { Auth };
--- src/ssr/index.tsx
+++ src/ssr/index.tsx
@@ -1,6 +1,7 @@
-import { ENV, Props } from "../common/common-types";
 import { fetchDecoratorReact, fetchDecoratorHtml, parseDecoratorHTMLToReact } from "./functions/ssr";
 import { injectDecoratorServerSide } from "./functions/ssr";
 import { Components, Elements } from "./functions/ssr";
+
 export { injectDecoratorServerSide, fetchDecoratorReact, fetchDecoratorHtml, parseDecoratorHTMLToReact };
-export type { ENV, Props, Elements, Components };
+
+export type { Elements, Components };
