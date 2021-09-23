ff3fd377e75720949bd7f23b75372df734b5ecc4
anders-nom <anders@nomerstad.com>
2021-09-23 11:08:39 +0200

    Optional url for breadcrumbs med handleinapp

--- src/common/common-types.ts
+++ src/common/common-types.ts
@@ -1,38 +1,48 @@
 export interface Language {
-    url: string;
-    locale: string;
-    handleInApp?: boolean;
+  url: string;
+  locale: string;
+  handleInApp?: boolean;
 }
 
-export interface Breadcrumb {
-    url: string;
-    title: string;
-    handleInApp?: boolean;
-}
+export type Breadcrumb =
+  | {
+      url?: string;
+      title: string;
+      handleInApp: true;
+    }
+  | {
+      url: string;
+      title: string;
+      handleInApp?: false | undefined;
+    };
 
 export type ENV = "localhost" | "prod" | "dev" | "q0" | "q1" | "q2" | "q6";
 
 export type Props = Params &
-    (
-        | { env: Exclude<ENV, "localhost"> }
-        | { env: Extract<ENV, "localhost">; port: number | string; dekoratorenUrl: string }
-        );
+  (
+    | { env: Exclude<ENV, "localhost"> }
+    | {
+        env: Extract<ENV, "localhost">;
+        port: number | string;
+        dekoratorenUrl: string;
+      }
+  );
 
 export interface Params {
-    context?: "privatperson" | "arbeidsgiver" | "samarbeidspartner";
-    simple?: boolean;
-    enforceLogin?: boolean;
-    redirectToApp?: boolean;
-    level?: string;
-    language?: "nb" | "nn" | "en" | "se" | "pl";
-    availableLanguages?: Language[];
-    breadcrumbs?: Breadcrumb[];
-    utilsBackground?: "white" | "gray" | "transparent";
-    feedback?: boolean;
-    chatbot?: boolean;
-    taSurveys?: string;
-    urlLookupTable?: boolean;
-    shareScreen?: boolean;
-    utloggingsvarsel?: boolean;
-    logoutUrl?: string
+  context?: "privatperson" | "arbeidsgiver" | "samarbeidspartner";
+  simple?: boolean;
+  enforceLogin?: boolean;
+  redirectToApp?: boolean;
+  level?: string;
+  language?: "nb" | "nn" | "en" | "se" | "pl";
+  availableLanguages?: Language[];
+  breadcrumbs?: Breadcrumb[];
+  utilsBackground?: "white" | "gray" | "transparent";
+  feedback?: boolean;
+  chatbot?: boolean;
+  taSurveys?: string;
+  urlLookupTable?: boolean;
+  shareScreen?: boolean;
+  utloggingsvarsel?: boolean;
+  logoutUrl?: string;
 }
