c6afdc3d15c1aa3eb15994cead4e055961210ffa
anders-nom <anders@nomerstad.com>
2021-10-04 14:37:03 +0200

    Bruker ikke peer dependencies

--- rollup.config.csr.js
+++ rollup.config.csr.js
@@ -1,31 +1,31 @@
 import typescript from "rollup-plugin-ts";
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
 
 const pkg = require("./package.json");
-let external = Object.keys(pkg.peerDependencies);
+let external = Object.keys(pkg.dependencies);
 
 export default {
-    input: ["src/csr/index.tsx"],
-    output: {
-        name: "csr",
-        file: "csr/index.js",
-        format: "umd",
-        sourcemap: true,
-        exports: "named",
-        globals: {
-            ["react"]: "React",
-        },
+  input: ["src/csr/index.tsx"],
+  output: {
+    name: "csr",
+    file: "csr/index.js",
+    format: "umd",
+    sourcemap: true,
+    exports: "named",
+    globals: {
+      ["react"]: "React",
     },
-    plugins: [
-        typescript({
-            tsconfig: baseConfig => ({
-                ...baseConfig,
-                outDir: "csr",
-                rootDirs: ['src/csr', 'src/common'],
-            }),
-            include: ['src/common/**/*.(ts|tsx)', 'src/csr/**/*.(ts|tsx)']
-        }),
-        terser()
-    ],
-    external: external,
+  },
+  plugins: [
+    typescript({
+      tsconfig: (baseConfig) => ({
+        ...baseConfig,
+        outDir: "csr",
+        rootDirs: ["src/csr", "src/common"],
+      }),
+      include: ["src/common/**/*.(ts|tsx)", "src/csr/**/*.(ts|tsx)"],
+    }),
+    terser(),
+  ],
+  external: external,
 };
