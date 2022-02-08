806bccf885fa6f0724b042b8a47a18aee54d0e77
Anders Bråthen Nomerstad <anders@nomerstad>
2022-02-08 16:06:11 +0100

    Hindrer undefined-verdier i query-string

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -9,17 +9,23 @@ export const naisUrls: { [env in NaisEnv]: string } = {
   dev: "https://dekoratoren.ekstern.dev.nav.no",
 };
 
+const objectToQueryString = (params: object) =>
+    params
+        ? Object.entries(params).reduce(
+            (acc, [k, v], i) =>
+                v !== undefined
+                    ? `${acc}${i ? '&' : '?'}${k}=${encodeURIComponent(
+                        typeof v === 'object' ? JSON.stringify(v) : v
+                    )}`
+                    : acc,
+            ''
+        )
+        : '';
+
 export const buildUrl = (url: string, params: Params, csr?: boolean) => {
   if (!params) {
     return url;
   }
 
-  return `${url}/${csr ? "env" : ""}?${Object.entries(params)
-    .map(
-      ([key, value]) =>
-        `${key}=${encodeURIComponent(
-          Array.isArray(value) ? JSON.stringify(value) : value
-        )}`
-    )
-    .join("&")}`;
+  return `${url}/${csr ? "env" : ""}${objectToQueryString(params)}`;
 };
