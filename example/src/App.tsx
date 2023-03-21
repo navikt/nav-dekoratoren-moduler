71c4ec57ef931c9b984e9e451383f0f3d459f3d5
Bjørn Harbo Dahle <bdahle@hey.com>
2023-03-21 13:59:05 +0100

    fiks typescript feilhåndtering

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,74 +9,85 @@ import { setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
 import { setParams } from "@navikt/nav-dekoratoren-moduler";
 
 const App = () => {
-  const initalParams = { simple: true };
-  const [error, setError] = useState();
-  const [localParams, setLocalParams] = useState<string>(
-    JSON.stringify(initalParams, null, 2)
-  );
-  const [activeLanguage, setActiveLanguage] = useState("nb");
+    const initalParams = { simple: true };
+    const [error, setError] = useState<any>();
+    const [localParams, setLocalParams] = useState<string>(
+        JSON.stringify(initalParams, null, 2)
+    );
+    const [activeLanguage, setActiveLanguage] = useState("nb");
 
-  onBreadcrumbClick((breadcrumb) => {
-    console.log(breadcrumb);
-  });
+    onBreadcrumbClick((breadcrumb) => {
+        console.log(breadcrumb);
+    });
 
-  onLanguageSelect((language) => {
-    console.log(`Active language: ${activeLanguage}`);
-    console.log(`Selected language: ${language.locale}`);
-    setActiveLanguage(language.locale);
-  });
+    onLanguageSelect((language) => {
+        console.log(`Active language: ${activeLanguage}`);
+        console.log(`Selected language: ${language.locale}`);
+        setActiveLanguage(language.locale);
+    });
 
-  const onParamChange = () => {
-    try {
-      const params = JSON.parse(localParams);
-      setParams(params);
-    } catch (error) {
-      console.log(error.message);
-      setError(error);
-    }
-  };
+    const onParamChange = () => {
+        try {
+            const params = JSON.parse(localParams);
+            setParams(params);
+        } catch (error) {
+            let message;
+            if (error instanceof Error) message = error.message;
+            else message = String(error);
+            console.log(message);
+            setError(error);
+        }
+    };
 
-  const authCallback = (auth: Auth) => {
-    console.log(auth);
-    setBreadcrumbs([
-      {
-        title: "Ditt nav",
-        url: "https://www.nav.no/person/dittnav",
-        handleInApp: true,
-      },
-      {
-        title: "Kontakt oss",
-        url: "https://www.nav.no/person/kontakt-oss",
-        handleInApp: true,
-      },
-    ]);
-    setAvailableLanguages([
-      { locale: "nb", url: "https://www.nav.no/no/person", handleInApp: true },
-      { locale: "en", url: "https://www.nav.no/en/person", handleInApp: true },
-    ]);
-  };
+    const authCallback = (auth: Auth) => {
+        console.log(auth);
+        setBreadcrumbs([
+            {
+                title: "Ditt nav",
+                url: "https://www.nav.no/person/dittnav",
+                handleInApp: true,
+            },
+            {
+                title: "Kontakt oss",
+                url: "https://www.nav.no/person/kontakt-oss",
+                handleInApp: true,
+            },
+        ]);
+        setAvailableLanguages([
+            {
+                locale: "nb",
+                url: "https://www.nav.no/no/person",
+                handleInApp: true,
+            },
+            {
+                locale: "en",
+                url: "https://www.nav.no/en/person",
+                handleInApp: true,
+            },
+        ]);
+    };
 
-  // console.log(header);
+    // console.log(header);
 
-  return (
-    <EnforceLoginLoader authCallback={authCallback}>
-      <div className={"testapp__container"}>
-        <div>Test app</div>
-        <div>
-          <textarea
-            value={localParams}
-            className={"testapp__textarea"}
-            placeholder={"params"}
-            onChange={(e) => setLocalParams(e.target?.value)}
-          />
-        </div>
-        <div>{error}</div>
-        <div>
-          <button onClick={() => onParamChange()}>Sett params</button>
-        </div>
-      </div>
-    </EnforceLoginLoader>
-  );
+    return (
+        <EnforceLoginLoader authCallback={authCallback}>
+            <div className={"testapp__container"}>
+                <div>Test app</div>
+                <div>
+                    <textarea
+                        value={localParams}
+                        className={"testapp__textarea"}
+                        placeholder={"params"}
+                        onChange={(e) => setLocalParams(e.target?.value)}
+                    />
+                </div>
+                <div>{error}</div>
+                <div>
+                    <button onClick={() => onParamChange()}>Sett params</button>
+                </div>
+            </div>
+        </EnforceLoginLoader>
+    );
 };
 
 export default App;
