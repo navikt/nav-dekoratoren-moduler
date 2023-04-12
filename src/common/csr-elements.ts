9cec584e05a44b74f14b41590dee0dcd2edf078c
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-04-12 14:49:31 +0200

    ?!

--- src/common/csr-elements.ts
+++ src/common/csr-elements.ts
@@ -13,11 +13,11 @@ export const getCsrElements = (csrProps: DecoratorCSRProps) => {
     const scriptSrc = `${assetsUrl}/client.js`;
 
     return {
-        header: '<div id="decorator-header"/>',
-        footer: '<div id="decorator-footer"/>',
-        env: `<div id="decorator-env" data-src="${envUrl}"/>`,
+        header: '<div id="decorator-header"></div>',
+        footer: '<div id="decorator-footer"></div>',
+        env: `<div id="decorator-env" data-src="${envUrl}"></div>`,
         styles: `<link href="${assetsUrl}/css/client.css" rel="stylesheet" />`,
-        scripts: `<script src="${scriptSrc}" async/>`,
+        scripts: `<script src="${scriptSrc}" async></script>`,
         scriptSrc,
     };
 };
