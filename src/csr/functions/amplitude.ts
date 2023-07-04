1b09f599338d0c05d8befc0b8e5ad6497e451765
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-07-04 14:25:10 +0200

    Removed JSDoc for now

--- src/csr/functions/amplitude.ts
+++ src/csr/functions/amplitude.ts
@@ -28,45 +28,6 @@ type AutocompleteString = string & {};
 export type AmplitudeEventName = AmplitudeEvent["name"];
 export type AutocompleteEventName = AmplitudeEventName | AutocompleteString;
 
-/**
- * This function logs an event to Amplitude. It validates the Amplitude instance before logging the event.
- *
- * @param params - An object containing the parameters for the event.
- * @param params.eventName - The name of the event to log, extends from {@link AutocompleteEventName}.
- * @param params.eventData - The data of the event, it's an optional parameter. If `eventName` is an instance of {@link AmplitudeEventName}, it should match the corresponding event's properties.
- * @param params.origin - The origin of the event, usually the component or module where the event was triggered.
- *
- * @throws Will throw an error if called server-side.
- * @throws Will throw an error if Amplitude instance is not found or has not been initialized yet.
- *
- * @returns A promise that resolves with the result of the `dekoratorenAmplitude` function.
- *
- * @see {@link AccordianÅpnet}
- * @see {@link AccordianLukket}
- * @see {@link AlertVist}
- * @see {@link Besøk}
- * @see {@link ChatAvsluttet}
- * @see {@link ChatStartet}
- * @see {@link FilterValg}
- * @see {@link GuidePanelVist}
- * @see {@link LastNed}
- * @see {@link ModalLukket}
- * @see {@link ModalÅpnet}
- * @see {@link Navigere}
- * @see {@link SkjemaFullført}
- * @see {@link SkjemaInnsendingFeilet}
- * @see {@link SkjemaSpørsmålBesvart}
- * @see {@link SkjemaStartet}
- * @see {@link SkjemaStegFullført}
- * @see {@link SkjemaValideringFeilet}
- * @see {@link SkjemaÅpnet}
- * @see {@link Søk}
- *
- *
- * @async
- * @function
- * @exports logAmplitudeEvent
- */
 export async function logAmplitudeEvent<
     TName extends AutocompleteEventName
 >(params: {
