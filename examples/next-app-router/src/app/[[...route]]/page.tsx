033cc5f39dd9dbd9f522c74cae86c7dc0ea71cf8
Anders Nomerstad <anders@nomerstad.com>
2024-09-13 09:17:33 +0200

    Rydder i eksempler

--- examples/next-app-router/src/app/[[...route]]/page.tsx
+++ examples/next-app-router/src/app/[[...route]]/page.tsx
@@ -2,25 +2,37 @@ import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-export default async function Home(props: { params: { route: string[] } }) {
-    const { params } = props;
+type PageProps = {
+    title: string;
+    content?: string;
+};
 
+const pagePropsByPath: Record<string, PageProps> = {
+    "/": {
+        title: "Privatperson",
+    },
+    "/arbeidsgiver": {
+        title: "Arbeidsgiver",
+    },
+    "/samarbeidspartner": {
+        title: "Samarbeidspartner",
+    },
+};
+
+export default async function Home({ params }: { params: { route: string[] } }) {
     const path = params.route?.join("/") || "";
 
     console.log(`Fetching from ${path}`);
 
-    const pageProps = await fetch(
-        `http://localhost:8080/_/service/no.nav.navno/sitecontent?id=/www.nav.no/${path}`,
-        { headers: { secret: "dummyToken" } },
-    ).then((res) => res.json());
+    const pageProps = pagePropsByPath[path];
 
     return (
         <main className={styles.main}>
             <Link href={"/"}>{"Privatperson"}</Link>
             <Link href={"/arbeidsgiver"}>{"Arbeidsgiver"}</Link>
             <Link href={"/aap"}>{"AAP"}</Link>
-            <h1>{pageProps.displayName}</h1>
-            {/*<code>{JSON.stringify(pageProps)}</code>*/}
+            {<h1>{pageProps ? pageProps.title : `Fant ikke siden for ${path}`}</h1>}
+            {pageProps && <p>{pageProps.content}</p>}
         </main>
     );
 }
